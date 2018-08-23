f5f32baaff5ef4b4630882c975e9c4ee852250c9
altnova <arina@lateralsummer.com>
2018-08-23 23:20:05 +0300

    header for del_s.c

---
+++ src/del_s.h
@@ -0,0 +1,24 @@
+
+ext V depict(S filename, I x, I y);
+
+ext V dog_sit(I pause);
+
+ext V dog_run(I pause);
+
+ext V dog_walk(I pause);
+
+ext V dog_sleep_1(I pause);
+
+ext V dog_sleep_2(I pause);			//<	???????
+												//<	both needs running back
+ext V dog_eat(I pause); 			//<	???????
+
+ext V dog_read(I pause);
+
+ext V dog_die(I pause);
+
+ext V dog_rise(I pause);
+
+ext V dog_love(I pause);
+
+ext V dog_poop(I pause);
