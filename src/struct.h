370f9fb00abc8c0357f18b68d29521bd1916aa32
altnova <arina@lateralsummer.com>
2018-10-25 20:53:00 +0300

    [~]

---
+++ src/struct.h
@@ -0,0 +1,11 @@
+// #include "base/cfg.h"
+
+ext V cnt_upd(tm_cnt cnt_, I num);									//< cnt++ for ex. after action
+
+ext V cnt_check();										//<	conditions of modifing dt in case of appropriate  cnt
+
+ext C death();											//<	conditions of exit 	
+
+// ext V event_check();									//< user's commands 
+
+ext C set_main_action();								//< set main action if nothing special happens
