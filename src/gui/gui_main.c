9f2fe7c1c8647f5a85cf3481d88e8f72b2a79c64
altnova <arina@lateralsummer.com>
2018-10-25 20:52:25 +0300

    main for png/gtk testing

---
+++ src/gui/gui_main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <gtk/gtk.h>
+#include "../___.h"
+#include "../png/canvas.h"
+#include "win.h"
+
+
+I main()
+{
+
+	/*	come code here about widgets and png viewer	*/
+
+	R 0;
+}
