92c73287f75ab5c1be8593e3b60bbf735249631c
altnova <arina@lateralsummer.com>
2018-10-28 03:16:07 +0300

    [+] add_to_canvas()

--- src/png/canvas.h
+++ src/png/canvas.h
@@ -1,3 +1,4 @@
 
+ext void add_to_canvas(I am, S* filename, I* x_, I* y_);			//< include filename's pics canvas.png and puts in into tmp.png
 
 ext void frame(I am, S* filename, I* x_, I* y_);			//< include filename's pics canvas.png and puts in into tmp.png
