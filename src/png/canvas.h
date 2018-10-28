707407e7c53bdab9d61a0d3b64dd1ef38259df31
altnova <arina@lateralsummer.com>
2018-10-28 03:41:15 +0300

    [+] set_canvas()

--- src/png/canvas.h
+++ src/png/canvas.h
@@ -2,3 +2,5 @@
 ext void add_to_canvas(I am, S* filename, UH* x_, UH* y_);			//< include filename's pics canvas.png and puts in into tmp.png
 
 ext void frame(I am, S* filename, UH* x_, UH* y_);			//< include filename's pics canvas.png and puts in into tmp.png
+
+ext void set_canvas();
