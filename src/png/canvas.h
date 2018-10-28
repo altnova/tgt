4fbe84acf14aa1e384edaeb41f73bd1158cc1e42
altnova <arina@lateralsummer.com>
2018-10-28 03:20:24 +0300

    [~]

--- src/png/canvas.h
+++ src/png/canvas.h
@@ -1,4 +1,4 @@
 
-ext void add_to_canvas(I am, S* filename, I* x_, I* y_);			//< include filename's pics canvas.png and puts in into tmp.png
+ext void add_to_canvas(I am, S* filename, UH* x_, UH* y_);			//< include filename's pics canvas.png and puts in into tmp.png
 
-ext void frame(I am, S* filename, I* x_, I* y_);			//< include filename's pics canvas.png and puts in into tmp.png
+ext void frame(I am, S* filename, UH* x_, UH* y_);			//< include filename's pics canvas.png and puts in into tmp.png
