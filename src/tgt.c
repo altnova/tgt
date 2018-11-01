b9e9b0c8903dc7ac8a90488af0f90f42d87e3159
altnova <arina@lateralsummer.com>
2018-11-01 20:39:45 +0300

    [~]

--- src/tgt.c
+++ src/tgt.c
@@ -5,14 +5,16 @@
 #include "logic/struct.h"
 
 #include "base/set.h"
+
 #include "logic/draw.h"
 #include "png/canvas.h"
+#include "base/mains.h"
 
 tm_cnt cnt;
 dat dt;
 coor crd;
 
-I stat_time[12] = 	{	1000000, 1000000, 1000000, 	1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 0};
+I stat_time[12] = 	{	250000, 250000,250000, 750000,1000000, 1750000,1750000, 250000, 	250000,  	250000, 750000, 	0};		//< div /2
 S stat_name[12] = 	{	"sit", "run", "walk", "eat", "read_", "die", "rise", "sleep_1", "sleep_2", 	"love", "wc", "return"};
 
 D_STAT d_stat;
@@ -23,23 +25,41 @@ I width;
 
 I main()
 {
+
+	I i, j = -1, k = 0;
 	set_start(dt, cnt, crd);
 
-	ser_canvas();
-	W(dt->action != rise && dt->action != die) {
+	set_canvas();
+	// R0;
+
+
+	// set_main_action();
+	// draw("dog", dt->action);
+	
+
+
+	for (i = 0; i < 100 && dt->action != rise && dt->action != die; i++) {
 		
 		set_main_action();									//< 					logic/struct.c
+		if (j != dt->action && j != -1)
+			k = i;
+		j = dt->action;
+
 		draw("dog", dt->action);							//<	draw action 		logic/draw.c
 		cnt_upd(cnt, dt->action);							//<	increase timers 	logic/struct.c
 
-		// event_check();									//< eat event to update dat and timers
+		event_check();									//< eat event to update dat and timers
 		cnt_check();										//< checking timers for updating dat 
 															//<						logic/struct.c
 
+		// R 0;
 		if (death())										//<	conditions of death
 			break;
 	}
 	
+	 p_dog_stat(cnt, dt);
+
+	 O("AT %d ACT CHANGED\n", k);
 	O("DIE draw action %s\n", stat_name[dt->action]);
 	O("END\n");
 	R0;
