db7d3ac21190189bdbde604bfda5f32dae795236
altnova <arina@lateralsummer.com>
2018-11-20 06:20:58 +0300

    [~]

--- src/gui/win.c
+++ src/gui/win.c
@@ -4,34 +4,25 @@
 
 #include "../png/canvas.h"
 
+
 V depict(I am, S *filename, UH *x_, UH *y_)										//<!!! 	GTK+
 {
 	// O("\tDEPICT [%s] at %dx%d\n", filename[0], x_[0], y_[0]);
-	// O("DEPICT\n");
 	frame(am, filename, x_, y_);									//< for tests!!!!
 }
 
-
-
-
 V set_win()
 {
-
 	/* make transparent (click/view) window fullscreen	*/
 
-
-
 	/* draw png on it	*/
-
 }
-
 
 I height_()
 {
 	height = 300;
 	R height;
 }
-
 
 I width_()
 {
