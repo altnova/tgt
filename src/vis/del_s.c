1ed8e8d4d347ff7270549d3321e7d57f3b147f6b
altnova <arina@lateralsummer.com>
2018-11-22 01:05:20 +0300

    [+] nice comments

--- src/vis/del_s.c
+++ src/vis/del_s.c
@@ -10,11 +10,39 @@
 #include "../gui/win.h"
 #include "../png/canvas.h"
 
-V col_dep_sleep(S filename, C col, I am_f, S* f_ptr, UH* x, UH* y, I pause);
 
-V this_way(I pause, S file_1, S file_2, S act, I way)			//< which way to move
+///////////////////////////////////////////////////////////////
+//<	col_dep_slp(S file, C c, I am, S* f, UH* x, UH* y, I p)	>//
+///////////////////////////////////////////////////////////////
+//<			colour() depict() and usleep()					>//
+//<			S file	-- 	file name to pass to colour()		>//
+//<			C c   	-- 	colour number to pass to colour()	>//
+//<			I am 	-- 	amount of files for depict()		>//
+//<			S* f 	--	list of files for depict()			>//
+//<			UH* x 	--	list of x coors for depict()		>//
+//<			UH* y 	--	list of y coors for depict() 		>//
+//<			I p 	--	pause for usleep					>//
+///////////////////////////////////////////////////////////////
+V col_dep_slp(S file, C c, I am, S* f, UH* x, UH* y, I p)
 {
-	col_dep_sleep(file_1, dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
+	colour(file, c);
+	depict(am, f, x, y);
+	usleep(p * SEC);
+}
+
+///////////////////////////////////////////////////////////////
+//<		this_way(I pause, S file_1, S file_2, S act, I way)	>//
+///////////////////////////////////////////////////////////////
+//<			choose which way to run 						>//
+//<			modify coordinates 								>//
+//<			S file_1 --> walk_?_1 or run_?_1 				>//
+//<			S file_2 --> walk_?_2 or run_?_2 				>//
+//<			S act 	 --> name of dog act 					>//
+//<			I way 	 --> direction [1/0]					>//	
+///////////////////////////////////////////////////////////////
+V this_way(I pause, S file_1, S file_2, S act, I way)			
+{
+	col_dep_slp(file_1, dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
 	crd->dir = way;
 
 	if (way)
@@ -22,31 +50,56 @@ V this_way(I pause, S file_1, S file_2, S act, I way)			//< which way to move
 	else 
 		crd->dog_x -= (act[0] == 'r') ? DIFRUN : DIFWALK;
 
-	col_dep_sleep(file_2, dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
+	col_dep_slp(file_2, dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_sir(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act sit with total pause I pause		>//
+///////////////////////////////////////////////////////////////
 V dog_sit(I pause)						
 {
-	col_dep_sleep("pic/dog/sit_1.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
-	col_dep_sleep("pic/dog/sit_2.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
+	col_dep_slp("pic/dog/sit_1.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
+	col_dep_slp("pic/dog/sit_2.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_run(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act run with total pause I pause		>//
+///////////////////////////////////////////////////////////////
 V dog_run(I pause)
 {
 	(crd->dog_x <= L_LIM || (crd->dog_x < R_LIM && crd->dir)) 	?						
-				this_way((pause*SEC), "pic/dog/run_r_1.png", "pic/dog/run_r_2.png", "run", RIGHT)		//<	run RIGHT
+				this_way((pause*SEC), 	"pic/dog/run_r_1.png", 
+										"pic/dog/run_r_2.png", "run", RIGHT)		//<	run RIGHT
 																:
-				this_way((pause*SEC), "pic/dog/run_l_1.png", "pic/dog/run_l_2.png", "run", LEFT);		//< run LEFT
+				this_way((pause*SEC), 	"pic/dog/run_l_1.png", 
+										"pic/dog/run_l_2.png", "run", LEFT);		//< run LEFT
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_walk(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act walk with total pause I pause	>//
+///////////////////////////////////////////////////////////////
 V dog_walk(I pause)
 {
 	(crd->dog_x <= L_LIM || (crd->dog_x < R_LIM && crd->dir)) 	?
-				this_way((pause*SEC), "pic/dog/walk_r_1.png", "pic/dog/walk_r_2.png", "walk", RIGHT)	//< walk RIGHT
+				this_way((pause*SEC), 	"pic/dog/walk_r_1.png", 
+										"pic/dog/walk_r_2.png", "walk", RIGHT)	//< walk RIGHT
 																:
-				this_way((pause*SEC), "pic/dog/walk_l_1.png", "pic/dog/walk_l_2.png", "walk", LEFT);	//<	walk LEFT
+				this_way((pause*SEC), 	"pic/dog/walk_l_1.png", 
+										"pic/dog/walk_l_2.png", "walk", LEFT);	//<	walk LEFT
 }
 
+///////////////////////////////////////////////////////////////
+//<						dog_return()						>//
+///////////////////////////////////////////////////////////////
+//<			choose the way of comming back to kennel 		>//
+//<					depends on tirednes						>//
+///////////////////////////////////////////////////////////////
 V dog_return()
 {
 	// (dt->satiety > STLIM && dt->cleanliness > CLLIM) 	? dog_run_back() :  dog_walk_back();
@@ -72,20 +125,39 @@ V dog_return()
 	}
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_sleep_1(I pause)					>//
+///////////////////////////////////////////////////////////////
+//<		depict dog act sleep_1 with total pause I pause		>//
+///////////////////////////////////////////////////////////////
 V dog_sleep_1(I pause)									//< outside [no strength to return]
 {
-	col_dep_sleep("pic/dog/sleep_1_1.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
-	col_dep_sleep("pic/dog/sleep_1_2.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
+	col_dep_slp("pic/dog/sleep_1_1.png", dt->colour, 1, ADDR_ADDR, 
+										&crd->dog_x, &crd->dog_y, pause/2);
+	col_dep_slp("pic/dog/sleep_1_2.png", dt->colour, 1, ADDR_ADDR, 
+										&crd->dog_x, &crd->dog_y, pause/2);
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_sleep_2(I pause)					>//
+///////////////////////////////////////////////////////////////
+//<		depict dog act sleep_2 with total pause I pause		>//
+///////////////////////////////////////////////////////////////
 V dog_sleep_2(I pause)									//< in kennel
 {
 	dog_return();
 
-	col_dep_sleep("pic/dog/sleep_2_1.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
-	col_dep_sleep("pic/dog/sleep_2_2.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/2);
+	col_dep_slp("pic/dog/sleep_2_1.png", dt->colour, 1, ADDR_ADDR, 
+										&crd->dog_x, &crd->dog_y, pause/2);
+	col_dep_slp("pic/dog/sleep_2_2.png", dt->colour, 1, ADDR_ADDR, 
+										&crd->dog_x, &crd->dog_y, pause/2);
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_eat(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act eat with total pause I pause		>//
+///////////////////////////////////////////////////////////////
 V dog_eat(I pause)
 {
 	I i, p = pause/7;
@@ -100,13 +172,19 @@ V dog_eat(I pause)
 	usleep(p*SEC);
 
 	DO(3, 	{  	
-
-			col_dep_sleep("pic/dog/eat_1.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);
-			col_dep_sleep("pic/dog/eat_2.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);});
+			col_dep_slp("pic/dog/eat_1.png", dt->colour, 1, ADDR_ADDR, 
+											&crd->dog_x, &crd->dog_y, p);
+			col_dep_slp("pic/dog/eat_2.png", dt->colour, 1, ADDR_ADDR, 
+											&crd->dog_x, &crd->dog_y, p);});
 
 	set_canvas();
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_read(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act read with total pause I pause	>//
+///////////////////////////////////////////////////////////////
 V dog_read(I pause)
 {
 	I i, p = pause/12;
@@ -114,14 +192,22 @@ V dog_read(I pause)
 	O("%s DOG_READ%s\n", CBLU, CNRM);
 
 	DO(3,  {	
-
-			col_dep_sleep("pic/dog/read_1.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);
-			col_dep_sleep("pic/dog/read_2.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);
-			col_dep_sleep("pic/dog/read_3.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);
-			col_dep_sleep("pic/dog/read_4.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);});
+			col_dep_slp("pic/dog/read_1.png", dt->colour, 1, ADDR_ADDR, 
+												&crd->dog_x, &crd->dog_y, p);
+			col_dep_slp("pic/dog/read_2.png", dt->colour, 1, ADDR_ADDR, 
+												&crd->dog_x, &crd->dog_y, p);
+			col_dep_slp("pic/dog/read_3.png", dt->colour, 1, ADDR_ADDR, 
+												&crd->dog_x, &crd->dog_y, p);
+			col_dep_slp("pic/dog/read_4.png", dt->colour, 1, ADDR_ADDR, 
+												&crd->dog_x, &crd->dog_y, p);});
 }
 
-V dog_die(I pause)				//< 	??????
+///////////////////////////////////////////////////////////////
+//<					dog_die(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act die with total pause I pause		>//
+///////////////////////////////////////////////////////////////
+V dog_die(I pause)			
 {
 	I i, p = pause/9, len;
 
@@ -130,9 +216,15 @@ V dog_die(I pause)				//< 	??????
 
 	DO(9, { 
 			DOG_FILE[12] = i + '1';
-			col_dep_sleep(DOG_FILE, dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);});			
+			col_dep_slp(DOG_FILE, dt->colour, 1, ADDR_ADDR, 
+									&crd->dog_x, &crd->dog_y, p);});			
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_rise(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act rise with total pause I pause	>//
+///////////////////////////////////////////////////////////////
 V dog_rise(I pause)				
 {
 	I i, p = pause/9;
@@ -143,31 +235,40 @@ V dog_rise(I pause)
 
 	DO(9, {	
 			DOG_FILE[19] = i + '1';
-			col_dep_sleep(DOG_FILE, dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);});
+			col_dep_slp(DOG_FILE, dt->colour, 1, ADDR_ADDR, 
+									&crd->dog_x, &crd->dog_y, p);});
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_love(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act love with total pause I pause	>//
+///////////////////////////////////////////////////////////////
 V dog_love(I pause)
 {
 	I i, p = pause/6;
 	
 	O("%sDOG_LOVE%s\n", CBLU, CNRM);
 	DO(3, {	
-			col_dep_sleep("pic/dog/love_1.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);
-			col_dep_sleep("pic/dog/love_2.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, p);});
+			col_dep_slp("pic/dog/love_1.png", dt->colour, 1, ADDR_ADDR, 
+											&crd->dog_x, &crd->dog_y, p);
+			col_dep_slp("pic/dog/love_2.png", dt->colour, 1, ADDR_ADDR, 
+											&crd->dog_x, &crd->dog_y, p);});
 }
 
+///////////////////////////////////////////////////////////////
+//<					dog_poop(I pause)						>//
+///////////////////////////////////////////////////////////////
+//<			depict dog act poop with total pause I pause	>//
+///////////////////////////////////////////////////////////////
 V dog_poop(I pause)
 {
 	O("%s DOG_UNPLEASED%s\n", CRED, CNRM);
 
-	col_dep_sleep("pic/dog/poop_1.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/3);
-	col_dep_sleep("pic/dog/poop_2.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/3);
-	col_dep_sleep("pic/dog/poop_3.png", dt->colour, 1, ADDR_ADDR, &crd->dog_x, &crd->dog_y, pause/3);
-}
-
-V col_dep_sleep(S filename, C col, I am_f, S* f_ptr, UH* x, UH* y, I pause)
-{
-	colour(filename, col);
-	depict(am_f, f_ptr, x, y);
-	usleep(pause * SEC);
+	col_dep_slp("pic/dog/poop_1.png", dt->colour, 1, ADDR_ADDR, 
+										&crd->dog_x, &crd->dog_y, pause/3);
+	col_dep_slp("pic/dog/poop_2.png", dt->colour, 1, ADDR_ADDR, 
+										&crd->dog_x, &crd->dog_y, pause/3);
+	col_dep_slp("pic/dog/poop_3.png", dt->colour, 1, ADDR_ADDR, 
+										&crd->dog_x, &crd->dog_y, pause/3);
 }
