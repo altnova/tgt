e7e7fbeda0f9d068f044d48f6d4978e6ea25f9af
altnova <arina@lateralsummer.com>
2018-11-22 00:02:03 +0300

    [+] improved env var check

--- src/bsc/set.c
+++ src/bsc/set.c
@@ -8,6 +8,7 @@
 #include "../cfg/cfg.h"
 #include "../gui/win.h"
 #include "../fld/wfile.h"
+#include "../png/canvas.h"
 #include "mains.h"
 
 /*
@@ -46,49 +47,68 @@ typedef struct Coor 		//< coordinates of objects
 #define BAR_H 0
 //<	!!!!!!!!!!!!!!!!!
 
+
+
+
+///////////////////////////////////////////////////////////////
+//<					global dog data structs 				>//
 tm_cnt cnt;
 dat dt;
 coor crd;
 
-I stat_time[12] = 	{	1000, 	1000,	1000,   3000, 4000,   7000,	  7000, 	1000, 	  1000,  	 1000,   3000, 		0 };		//< /1000 --> get am in seconds 
-S stat_name[12] = 	{	"sit", "run", "walk", "eat", "read_", "die", "rise", "sleep_1", "sleep_2", 	"love", "wc", "return"};
+//<	stat_time contains all intervals for each dog action	>//
+I stat_time[12] = {	1000, 1000, 1000, 3000, 4000, 7000, 
+					7000, 1000, 1000, 1000, 3000, 0 };		//< /1000 --> get am in seconds 
+S stat_name[12] = {	"sit", "run", "walk", "eat", "read_", 
+					"die","rise","slp_1","slp_2","love","wc","R"};
 
+//<					enum for dog actions					>//
 D_STAT d_stat;
 
+//<				height and width of a screen				>//
 I height;
 I width;
 
+//<							buffers							>//
 C NUM_INT[12];
 C DOG_FILE[LINE_MAX_];
 C FILENAME[LINE_MAX_];
 S ADDR = FILENAME;
 S* ADDR_ADDR = &ADDR;
 
+//<						start dog data 						>//
 pDat DAT = {5, 5, 5, 2, 9};
 pCounter CNT = {0,  0, 0, 0};
 pCoor COR;
+//<															>//
+///////////////////////////////////////////////////////////////
 
 /*
-typedef struct Cnt_tm		//< counters
+Cnt:		//< counters
 {
 	I last_act;
 	I satiety;
 	I intellect;
 	I cleanliness;
-} pCounter;
+} 
 
-
-typedef struct Dt 			//< dog info
+Dt:			//< dog info
 {
 	C satiety;
 	C intellect;
 	C cleanliness;
 	C colour;
 	C action;
-} pDat;
-
+} 
 */
 
+
+///////////////////////////////////////////////////////////////
+//<	set_data(dat dt_main, tm_cnt cnt_main, coor crd_main)	>//
+///////////////////////////////////////////////////////////////
+//<		set global variables	for ex: 					>//
+//<		width, height, all coordinates, struct pointers		>//
+///////////////////////////////////////////////////////////////
 V set_data(dat dt_main, tm_cnt cnt_main, coor crd_main)
 {
 	height = height_();
@@ -111,7 +131,7 @@ V set_data(dat dt_main, tm_cnt cnt_main, coor crd_main)
 	crd_main->dog_x = 200;
 	// crd_main->bar_x 	= R_LIM;
 	crd_main->bar_x 	= 600;
-	// crd_main->int_x 	= R_LIM + BAR_W - NOM_W;	//<	BAR_W ??? NOM_W ???
+	// crd_main->int_x 	= R_LIM + BAR_W - NOM_W;		//<	BAR_W ??? NOM_W ???
 	crd_main->int_x 	= crd_main->int_x;
 	crd_main->cle_x 	= crd_main->int_x;
 	crd_main->sat_x 	= crd_main->int_x;
@@ -119,7 +139,7 @@ V set_data(dat dt_main, tm_cnt cnt_main, coor crd_main)
 		/*	y 	*/
 	// crd_main->kennel_y 	= D_LIM - KENNEL_H;			//<	D_LIM ??? KENNEL_H		
 	crd_main->kennel_y 	= 100;
-	crd_main->bowl_y 	= D_LIM - BOWL_H;			//< BOWL_H ???
+	crd_main->bowl_y 	= D_LIM - BOWL_H;				//< BOWL_H ???
 	// crd_main->dog_y 	= D_LIM - DOG_H;				//<	DOG_H ????
 	crd_main->dog_y = 100;
 	// crd_main->bar_y 	= D_LIM - BAR_H;
@@ -134,62 +154,24 @@ V set_data(dat dt_main, tm_cnt cnt_main, coor crd_main)
 }
 
 
-V set_window()			//< fullscreen, transparent, click-trough
+///////////////////////////////////////////////////////////////
+//<						set_window()						>//
+///////////////////////////////////////////////////////////////
+//<			send call to some c++ functions to set  		>//
+//<		fullscreen transparent and click-through window 	>//
+///////////////////////////////////////////////////////////////
+V set_window()			
 {
 
 	/*	something from gui/win.c 	*/
 
 }
-																	//!!!!!<	GTK+
-V set_objects()		//< kennel, board, empty_bowl;
-{
 
-}
-
-
-C is_tgt(S path)
-{
-	I len = strlen(path);
-
-	if (!subcheck_d(path, "", len))
-		R 0;
-
-	if (!subcheck_d(path, "/pic/dog/0", len))					//< pic/dog check
-		R 0;
-	if (!subcheck_d(path, "/pic/dog/1", len))
-		R 0;
-	if (!subcheck_d(path, "/pic/dog/2", len))
-		R 0;
-	if (!subcheck_d(path, "/pic/dog/3", len))
-		R 0;
-	if (!subcheck_d(path, "/pic/dog/4", len))
-		R 0;
-
-	if (!subcheck_d(path, "/pic/obj", len))						//< pic/obj check
-		R 0;
-
-	if (!subcheck_d(path, "/pic/brd", len))						//<	pic/brd check
-		R 0;
-
-	R 1;
-}
-
-
-C is_home(S path)
-{
-	I len = strlen(path);
-
-
-	if (!subcheck_d(path, "", len))
-		R 0;
-
-	if (!subcheck_d(path, "/Desktop", len))			//< pic/dog check
-		R 0;
-	
-	R 1;
-}
-
-
+///////////////////////////////////////////////////////////////
+//<						check_env()							>//
+///////////////////////////////////////////////////////////////
+//<		check for TGT_HOME and HOME env var existence		>//
+///////////////////////////////////////////////////////////////
 V check_env()
 {
 	S tgt_home = getenv("TGT_HOME"), home = getenv("HOME");
@@ -208,22 +190,18 @@ V check_env()
 		O("go and fuck yourself\n");
 		exit(1);
 	}
-
-
 }
 
-/*tm_cnt cnt;
-dat dt;
-coor crd;
-*/
-
-
+///////////////////////////////////////////////////////////////
+//<		set_start(tm_cnt cnt_, dat dt_, coor crd_)			>//
+///////////////////////////////////////////////////////////////
+//<		main checks and set data into cnt_, dt_ and crd_ 	>//
+///////////////////////////////////////////////////////////////
 V set_start(tm_cnt cnt_, dat dt_, coor crd_)
 {
 	check_env();
 	set_data(dt_, cnt_, crd_);
 	set_window();
-	set_objects();
-	
+	set_canvas();	
 }
 
