34f1a7180fba5b3ed8555d082cad7001af256417
altnova <arina@lateralsummer.com>
2018-12-05 02:25:07 +0300

    [~]

--- src/gui/win.c
+++ src/gui/win.c
@@ -16,6 +16,8 @@ V depict(I am, S *filename, UH *x_, UH *y_)
 {
 	// O("\tDEPICT [%s] at %dx%d\n", filename[0], x_[0], y_[0]);
 	frame(am, filename, x_, y_);									//< for tests!!!!
+
+	//< imgbox_upd !!!
 }
 
 //<	may be unnecessary
